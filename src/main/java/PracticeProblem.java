956b654b88d828c5294fa7972b69463d1d0ac45c
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-03-22 21:35:57 +0000

    start

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,34 @@ public class PracticeProblem {
 
 	}
 
+
+
+
+	public static int goodString (String line)
+	{
+		int rightSide = 0;
+		int bigger=0;
+		int current=100;
+		for (int leftSide = 0 ; leftSide<line.length() ; leftSide++)
+		{
+
+			char letter = line.charAt(leftSide);
+			rightSide = line.length()-line.lastIndexOf(letter);
+			current = line.length()-(leftSide+rightSide);
+			if (current>=bigger)
+			{
+				bigger = current;
+			}
+	
+		}
+
+		if (bigger>=line.length())
+		{
+			return -1;
+		}
+
+
+		return bigger;
+	}
+
 }
